fb82f98095ef0d904861d959b2977c3eca0d9a44
Rafael Andrade <raafael4212@gmail.com>
2022-11-20 17:00:26 -0300

    cores e responsividade

--- src/app/component/Pedido de oração/pedidos-form/pedidos-form.component.ts
+++ src/app/component/Pedido de oração/pedidos-form/pedidos-form.component.ts
@@ -1,24 +1,28 @@
+import { getDatabase } from '@angular/fire/database';
 import { Component, OnInit } from '@angular/core';
 import {addDoc, Firestore, collection, getDocs, doc, updateDoc,deleteDoc} from '@angular/fire/firestore';
+import { databaseInstanceFactory } from '@angular/fire/database/database.module';
 
 @Component({
   selector: 'app-pedidos-form',
   templateUrl: './pedidos-form.component.html',
-  styleUrls: ['./pedidos-form.component.scss']
+  styleUrls: ['./pedidos-form.component.scss', '../../../cores.scss']
 })
 export class PedidosFormComponent {
+   myDate = new Date(Date.now()).toDateString()
 
   constructor(private firestore: Firestore ) {
   }
 
   criarPedido(value:any){
-    const dbInstance = collection(this.firestore, 'pedidos');
+    const dbInstance = collection(this.firestore, `pedidos`);
     addDoc(dbInstance, value).then(()=> {
       alert('enviado')
     })
     .catch((err)=> {
       alert(err.message)
     })
+    console.log(this.myDate);
   }
 
 
